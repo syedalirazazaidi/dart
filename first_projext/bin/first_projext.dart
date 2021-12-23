487c94da765073b705d9b563292fc192e1020515
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2021-12-23 23:46:11 +0500

    add arthimatic

--- first_projext/bin/first_projext.dart
+++ first_projext/bin/first_projext.dart
@@ -1,16 +1,44 @@
-// stringmethod to upper case
-void main(List<String> arguments) {
-  String name = 'i ma a software engineer';
+// ------------------Some Basic String Method------------//
+// void main(List<String> arguments) {
+//   String name = 'i ma a software engineer';
+//   print(name.toUpperCase());
+//   print(name.toLowerCase());
+//   print(name.contains('r'));
+//   print(name.replaceFirst('r', 'm'));
+//   print(name.replaceRange(1, 1, 'tr'));
+//   print(name);
+//   print(name.indexOf('.'));
+//   print(name.isNotEmpty);
+//   print(name.length);
+//   print(name.isEmpty);
+//   print(name.indexOf('i'));
+// }
 
-  print(name.toUpperCase());
-  print(name.toLowerCase());
-  print(name.contains('r'));
-  print(name.replaceFirst('r', 'm'));
-  print(name.replaceRange(1, 1, 'tr'));
-  print(name);
-  print(name.indexOf('.'));
-  print(name.isNotEmpty);
-  print(name.length);
-  print(name.isEmpty);
-  print(name.indexOf('i'));
+// Number in dart
+void main(List<String> args) {
+  // int list1 = 2;
+  // int list2 = 3;
+  // print(list1 + list2);
+  // print(list1 - list2);
+
+  // print(list1 / list2);
+  // print(list1 % list2);
+  // print(10.5 ~/ 10);
+  // int item = 10;
+  // item++;
+  // print(item++);
+  // print(item);
+  // item += item;
+  // print(item);
+  // int a = 3;
+  // print(--a);
+  // print(a);
+  String ab = 'aliraza';
+  String item = ab.toUpperCase();
+  print(item);
+  int abc = 24;
+  double itemDouble = abc.toDouble();
+  print(itemDouble);
+  itemDouble.toString();
+  print(itemDouble);
 }
