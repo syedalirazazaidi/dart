78517b67196166cabf2a9b42edd56ed553a1c842
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2021-12-23 14:08:39 +0500

    add string function:

--- first_projext/bin/first_projext.dart
+++ first_projext/bin/first_projext.dart
@@ -1,3 +1,16 @@
+// stringmethod to upper case
 void main(List<String> arguments) {
-  print('hi ali ups!');
+  String name = 'i ma a software engineer';
+
+  print(name.toUpperCase());
+  print(name.toLowerCase());
+  print(name.contains('r'));
+  print(name.replaceFirst('r', 'm'));
+  print(name.replaceRange(1, 1, 'tr'));
+  print(name);
+  print(name.indexOf('.'));
+  print(name.isNotEmpty);
+  print(name.length);
+  print(name.isEmpty);
+  print(name.indexOf('i'));
 }
